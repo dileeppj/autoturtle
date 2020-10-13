a1af48142d52cae3d8fbaeeb15afa3b4707bb7be
Dileep <pjdileep6@gmail.com>
2020-10-13 08:17:34 +0530

    [DEVEL] Complied the srv example code

---
+++ devel/share/basic_tutorials/cmake/basic_tutorials-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(basic_tutorials_MESSAGE_FILES "")
+set(basic_tutorials_SERVICE_FILES "/home/r00t/catkin_ws/src/basic_tutorials/srv/AddTwoInts.srv")
---
+++ devel/share/basic_tutorials/cmake/basic_tutorials-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(basic_tutorials_MSG_INCLUDE_DIRS "")
+set(basic_tutorials_MSG_DEPENDENCIES std_msgs)
